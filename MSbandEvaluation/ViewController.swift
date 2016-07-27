079db1a14e009294868c51cfdd3dfb42c82f146e
Katrin Hänsel <k.hansel@qmul.ac.uk>
2016-07-27 13:19:07 +0100

    First Version to give MacArena
    - Recording works 
    - Either one device or both
    - functionality to set markers

--- MSbandEvaluation/ViewController.swift
+++ MSbandEvaluation/ViewController.swift
@@ -10,16 +10,23 @@ import UIKit
 import PolarHRService
 import MSBandSensorService
 import SensorEvaluationShared
+import AudioToolbox
 
 class ViewController: UITableViewController, PeriphalEventHandler, MSBEventHandler, PolarEventHandler  {
 
+    @IBOutlet var polarTableRows: [UIView]!
     
+    @IBOutlet var msbTableRows: [UITableViewCell]!
+
+    @IBOutlet var recordingTableRows: [UITableViewCell]!
     
     @IBOutlet weak var connectPolarBtn: UIButton!
     
     @IBOutlet var connectMSBandBtn: UIButton!
     
-    @IBOutlet weak var recordDataBtn: UIButton!
+    @IBOutlet weak var startRecordingBtn: UIButton!
+    
+    @IBOutlet weak var stopRecordingBtn: UIButton!
     
     @IBOutlet weak var polarConnectionTextB: UILabel!
     
@@ -27,6 +34,9 @@ class ViewController: UITableViewController, PeriphalEventHandler, MSBEventHandl
     
     @IBOutlet weak var recordDataTextB: UILabel!
     
+    @IBOutlet weak var polarEnableSwitch: UISwitch!
+    
+    @IBOutlet weak var msbEnableSwitch: UISwitch!
     
     @IBOutlet weak var polarHROutputTextB: UILabel!
     
@@ -42,6 +52,50 @@ class ViewController: UITableViewController, PeriphalEventHandler, MSBEventHandl
     
     @IBOutlet weak var recordingTimeTextB: UILabel!
     
+    var _polarEnabled = true
+    var polarEnabled : Bool{
+        get{
+            return _polarEnabled
+        }
+        set(newVal){
+            if(!polarConnected && newVal != _polarEnabled){
+                _polarEnabled = newVal
+                
+                if(_polarEnabled){
+                    connectPolarBtn.enabled = true
+                    polarTableRows.map({(e) -> Void in e.hidden = false })
+                }else{
+                    connectPolarBtn.enabled = false
+                    polarTableRows.map({(e) -> Void in e.hidden = true })
+                }
+                
+                checkRecordDataValid()
+            }
+        }
+    }
+    
+    var _msbEnabled = true
+    var msbEnabled : Bool{
+        get{
+            return _msbConnected
+        }
+        set(newVal){
+            if(!msbConnected && newVal != _msbEnabled){
+                _msbEnabled = newVal
+                
+                if(_msbEnabled){
+                    connectMSBandBtn.enabled = true
+                    msbTableRows.map({(e) -> Void in e.hidden = false })
+                }else{
+                    connectMSBandBtn.enabled = false
+                    msbTableRows.map({(e) -> Void in e.hidden = true })
+                }
+                
+                checkRecordDataValid()
+            }
+        }
+    }
+
     
     var _polarConnected = false
     var polarConnected : Bool{
@@ -51,17 +105,13 @@ class ViewController: UITableViewController, PeriphalEventHandler, MSBEventHandl
         set(newVal){
             if(newVal != _polarConnected){
                 _polarConnected = newVal
-                if(_polarConnected){
-                    connectPolarBtn.titleLabel?.text = "Disconnect Polar Strap"
-                }else{
-                    connectPolarBtn.titleLabel?.text = "Connect Polar Strap"
-                }
+//                if(_polarConnected){
+//                    connectPolarBtn.titleLabel?.text = "Disconnect Polar Strap"
+//                }else{
+//                    connectPolarBtn.titleLabel?.text = "Connect Polar Strap"
+//                }
                 
-                if(_msbConnected && _polarConnected){
-                    recordDataBtn.enabled = true
-                }else{
-                    recordDataBtn.enabled = false
-                }
+                checkRecordDataValid()
             }
         }
     }
@@ -74,18 +124,36 @@ class ViewController: UITableViewController, PeriphalEventHandler, MSBEventHandl
         set(newVal){
             if(newVal != _msbConnected){
                 _msbConnected = newVal
-                if(_msbConnected){
-                    connectMSBandBtn.titleLabel?.text = "Disconnect MS Band"
-                }else{
-                    connectMSBandBtn.titleLabel?.text = "Connect MS Band"
-                }
+//                if(_msbConnected){
+//                    connectMSBandBtn.titleLabel?.text = "Disconnect MS Band"
+//                }else{
+//                    connectMSBandBtn.titleLabel?.text = "Connect MS Band"
+//                }
                 
-                if(_msbConnected && _polarConnected){
-                    recordDataBtn.enabled = true
-                }else{
-                    recordDataBtn.enabled = false
-                }
+                checkRecordDataValid()
             }
+        }
+    }
+    
+    private func checkRecordDataValid(){
+        var recordingCheck = false
+        
+        if(!polarConnected && !msbConnected){
+            recordingCheck = false
+        }else{
+            
+            let polarCheck = polarEnabled == polarConnected
+            let msbCheck = msbConnected == msbEnabled
+            
+            recordingCheck = polarCheck && msbCheck
+        }
+        
+        if(recordingCheck){
+            startRecordingBtn.enabled = true
+            recordDataTextB.textColor = UIColor.redColor()
+        }else{
+            startRecordingBtn.enabled = false
+            recordDataTextB.textColor = UIColor.grayColor()
         }
     }
     
@@ -98,17 +166,27 @@ class ViewController: UITableViewController, PeriphalEventHandler, MSBEventHandl
             if(newVal != _recordData){
                 _recordData = newVal
                 if(_recordData){
-                    recordDataBtn.titleLabel?.text = "Stop Recording"
-                    connectMSBandBtn.enabled = false
-                    connectPolarBtn.enabled = false
+//                    connectMSBandBtn.enabled = false
+//                    connectPolarBtn.enabled = false
+                    
+                    startRecordingBtn.enabled = false
+                    stopRecordingBtn.enabled = true
+                    
+                    recordingTableRows.map({(e) -> Void in e.hidden = false })
                 }else{
-                    recordDataBtn.titleLabel?.text = "Start Recording"
-                    connectMSBandBtn.enabled = true
-                    connectPolarBtn.enabled = true
+//                    connectMSBandBtn.enabled = true
+//                    connectPolarBtn.enabled = true
+                    
+                    startRecordingBtn.enabled = true
+                    stopRecordingBtn.enabled = false
+                    
+                    recordingTableRows.map({(e) -> Void in e.hidden = true })
                 }
             }
         }
     }
+    
+    
     
     
     override func viewDidLoad() {
@@ -121,10 +199,16 @@ class ViewController: UITableViewController, PeriphalEventHandler, MSBEventHandl
         PolarHRService.instance.subcribeToHREvents(self)
         PolarHRService.instance.subcribeToPeriphalEvents(self)
         
-        recordDataBtn.enabled = false
-
+        startRecordingBtn.enabled = false
+        
+        recordDataTextB.textColor = UIColor.grayColor()
+        
+        stopRecordingBtn.enabled = false
+        
+        recordingTableRows.map({(e) -> Void in e.hidden = true })
+        
         // Do any additional setup after loading the view, typically from a nib.
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: #selector(ViewController.addTapped))
+        //self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: #selector(ViewController.addTapped))
     }
     
     func addTapped(){
@@ -137,35 +221,98 @@ class ViewController: UITableViewController, PeriphalEventHandler, MSBEventHandl
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func polarEnabledValueChanged(sender: AnyObject) {
+        let enabled = (sender as! UISwitch).on
+        polarEnabled = enabled
+        
+    }
+    
+    
+    @IBAction func msbEnabledValueChanged(sender: AnyObject) {
+        let enabled = (sender as! UISwitch).on
+        msbEnabled = enabled
+    }
+    
 
     @IBAction func connectPolarBtnEvent(sender: AnyObject) {
         if(!polarConnected){
+            polarEnableSwitch.enabled = false
+            connectPolarBtn.enabled = false
             PolarHRService.instance.connect()
-        }else{
-            
         }
     }
 
     @IBAction func connectMSBandBtnEvent(sender: AnyObject) {
         if(!msbConnected){
+            msbEnableSwitch.enabled = false
+            connectMSBandBtn.enabled = false
             MSBService.instance.connect()
-        }else{
-            
         }
     }
     
-    @IBAction func recordDataBtnEvent(sender: AnyObject) {
+    
+    
+    @IBAction func startRecordingBtnEvent(sender: AnyObject) {
+        recordData = true
+        if !timer.valid {
+            let aSelector : Selector = "updateTime"
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: aSelector, userInfo: nil, repeats: true)
+            startTime = NSDate.timeIntervalSinceReferenceDate()
+        }
+    }
+    
+    @IBAction func stopRecodingBtnEvent(sender: AnyObject) {
+        recordData = false
+        DataStorage.sharedInstance.writeToDisk()
+        DataStorage.sharedInstance.reset()
+        timer.invalidate()
+    }
+    
+    var startTime = NSTimeInterval()
+    var timer = NSTimer()
+    
+    func updateTime() {
         
-        if(!recordData){
-            recordData = true
-        }
-        else{
-            recordData = false
-            DataStorage.sharedInstance.writeToDisk()
-            DataStorage.sharedInstance.reset()
-        }
+        var currentTime = NSDate.timeIntervalSinceReferenceDate()
+        
+        //Find the difference between current time and start time.
+        
+        var elapsedTime: NSTimeInterval = currentTime - startTime
+        
+        //calculate the minutes in elapsed time.
+        
+        let minutes = UInt8(elapsedTime / 60.0)
+        
+        elapsedTime -= (NSTimeInterval(minutes) * 60)
+        
+        //calculate the seconds in elapsed time.
+        
+        let seconds = UInt8(elapsedTime)
+        
+        elapsedTime -= NSTimeInterval(seconds)
+        
+        //find out the fraction of milliseconds to be displayed.
+        
+        //let fraction = UInt8(elapsedTime * 100)
+        
+        //add the leading zero for minutes, seconds and millseconds and store them as string constants
+        
+        let strMinutes = String(format: "%02d", minutes)
+        let strSeconds = String(format: "%02d", seconds)
+        //let strFraction = String(format: "%02d", fraction)
+        
+        //concatenate minuets, seconds and milliseconds as assign it to the UILabel
+        
+        recordingTimeTextB.text = "\(strMinutes):\(strSeconds)" //:\(strFraction)"
         
     }
+
+    @IBAction func placeMarkerBtnEvent(sender: AnyObject) {
+        DataStorage.sharedInstance.appendMarkerTimestamp()
+        AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
+    }
+    
     
     //MARK: PeriphalEventHandler
     
